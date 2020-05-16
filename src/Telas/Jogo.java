a53fadfe7a3a31b09b117ee17e1a122f172d43a1
NeitanDev <natan.miranda02@gmail.com>
2020-05-15 23:38:19 -0300

    sortea add

--- src/Telas/Jogo.java
+++ src/Telas/Jogo.java
@@ -6,6 +6,7 @@
 package Telas;
 
 import java.text.Normalizer.Form;
+import java.util.Random;
 import javax.swing.ImageIcon;
 
 /**
@@ -23,7 +24,42 @@ public class Jogo extends javax.swing.JFrame {
     }
 
     public void sortea(){
+        while(num<17){
+            int lin = ran.nextInt(4);
+            int col = ran.nextInt(8);
+            boolean existe = true;
+            
+            while(existe==true){
+                if(m[lin][col]==0){
+                    m[lin][col]=num;
+                    existe = false;
+                } else {
+                    lin = ran.nextInt(4);
+                    col = ran.nextInt(8);
+                }
+            }
+            existe = true;
+            lin = ran.nextInt(4);
+            col = ran.nextInt(8);
+            while(existe==true){
+                if(m[lin][col]==0){
+                    m[lin][col]=num;
+                    existe = false;
+                } else {
+                    lin = ran.nextInt(4);
+                    col = ran.nextInt(8);
+                }
+            }
         
+            num++;
+        }
+        
+        for(int i=0; i<4; i++){
+            for(int j=0; j<8;j++){
+                System.out.print(m[i][j]+"  ");
+            }
+            System.out.println("    ");
+        }
     }
     
     public void desconhecido(){
@@ -63,6 +99,10 @@ public class Jogo extends javax.swing.JFrame {
         jLabel35.setIcon(imagem1);
 //        jLabel36.setIcon(imagem1);
     }
+    
+    int m[][] = new int[4][8];
+    Random ran = new Random();
+    int num = 1;
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
